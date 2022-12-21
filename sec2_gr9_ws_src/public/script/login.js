9e6736404193e92e220755a236dad1d03ee94820
NNLoat <83104226+NNLoat@users.noreply.github.com>
2022-12-21 16:48:48 +0700

    Add files via upload

---
+++ sec2_gr9_ws_src/public/script/login.js
@@ -0,0 +1,27 @@
+
+ipv4 = "3.83.218.73";
+
+
+function CheckUser() {
+    let username = document.getElementById("Uname").value;
+    let password = document.getElementById("Pass").value;
+    let rooturl = "http://"+ipv4+":3030/login/"+username+"/"+password;
+    console.log(rooturl)
+    //console.log(username, password);
+
+    fetch(rooturl).then((res) => res.json()).then((data) => {
+        //console.log(data);
+        try {
+            alert("Welcome " + data.result.username);
+            document.location.href = "search.html";
+            
+        }
+        catch(error) {
+            var log = document.getElementById("login");
+            var error_log = document.createElement("div");
+            error_log.innerText = "Your username or password maybe incorrect";
+            error_log.style.color = "red"; 
+            log.appendChild(error_log);
+        }
+        })
+}
